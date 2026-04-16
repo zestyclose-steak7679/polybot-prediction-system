731d259a2a4411e5e3ffbaf728c9a69cda29021c
zestyclose-steak7679 <184917614+zestyclose-steak7679@users.noreply.github.com>
2026-04-16 01:57:54 +0000

    day4-task1: Revert polybot state file tracking and fix NoneType

--- meta/decision_engine.py
+++ meta/decision_engine.py
@@ -43,6 +43,8 @@ class DecisionEngine:
             has_conflict = False
             best_clv_agent = agent_id
             max_clv = agent_metrics.get(agent_id, {}).get("avg_clv", 0)
+            if max_clv is None:
+                max_clv = 0
 
             for other_sig in market_group:
                 conflicting_agents.append(other_sig.strategy)
@@ -51,8 +53,6 @@ class DecisionEngine:
                 other_clv = agent_metrics.get(other_sig.strategy, {}).get("avg_clv", 0)
                 if other_clv is None:
                     other_clv = 0
-                if max_clv is None:
-                    max_clv = 0
                 if other_clv > max_clv:
                     max_clv = other_clv
                     best_clv_agent = other_sig.strategy
